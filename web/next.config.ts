08287a50251682a54860d04f061851eda1edca8f
Cristian <cristianchaparroa@users.noreply.github.com>
2025-11-19 12:39:15 -0500

    fix web build (#3)

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    /* config options here */
+    reactStrictMode: true,
+    turbopack: {
+        root: process.cwd(),
+    },
+    webpack: (config) => {
+        config.resolve.alias = {
+            ...config.resolve.alias,
+            'pino-pretty': false,
+            '@react-native-async-storage/async-storage': false,
+        };
+        return config;
+    },
 };
 
 export default nextConfig;
