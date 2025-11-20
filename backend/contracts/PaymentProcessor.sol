b378f18f3e10b4c4811657b4e5338706f9ef4b92
Cristian Chaparro <cristianchaparroa@gmail.com>
2025-11-20 11:51:13 -0300

    delete node modules

--- backend/contracts/PaymentProcessor.sol
+++ backend/contracts/PaymentProcessor.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 /**
@@ -107,8 +107,9 @@ contract PaymentProcessor is ReentrancyGuard, Ownable {
 
     /**
      * @dev Inicializa el contrato
+     * @param initialOwner Dirección del propietario inicial del contrato
      */
-    constructor() {
+    constructor(address initialOwner) Ownable(initialOwner) {
         paymentCount = 0;
     }
 
